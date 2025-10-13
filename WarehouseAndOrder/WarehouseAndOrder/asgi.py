56136d073e9cb536b5c2b3d1fb94b2c0fd5fc7d3
ViNo24011 <email@example.com>
2025-10-13 21:45:24 +0700

    fix 1 chut

--- WarehouseAndOrder/WarehouseAndOrder/asgi.py
+++ WarehouseAndOrder/WarehouseAndOrder/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for khohangvadonhang project.
+ASGI config for WarehouseAndOrder project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'khohangvadonhang.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WarehouseAndOrder.settings')
 
 application = get_asgi_application()
--- WarehouseAndOrder/WarehouseAndOrder/wsgi.py
+++ WarehouseAndOrder/WarehouseAndOrder/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for khohangvadonhang project.
+WSGI config for WarehouseAndOrder project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'khohangvadonhang.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WarehouseAndOrder.settings')
 
 application = get_wsgi_application()
