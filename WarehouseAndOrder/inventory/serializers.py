4f566a29748f4d325ab19027caffb97891e5aef6
ViNo24011 <email@example.com>
2025-10-14 21:34:09 +0700

    btl_python

--- WarehouseAndOrder/inventory/serializers.py
+++ WarehouseAndOrder/inventory/serializers.py
@@ -1,18 +1,8 @@
 from rest_framework import serializers
 from .models import Category, Product, Warehouse
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = '__all__'
-
-class WarehouseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Warehouse
-        fields = '__all__'
 
 class ProductSerializer(serializers.ModelSerializer):
-    category = CategorySerializer(read_only=True)
     
     class Meta:
         model = Product
